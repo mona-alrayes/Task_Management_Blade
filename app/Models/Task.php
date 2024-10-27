768624a2dfcbdb7320deff88f80aadd2e39f108a
Mona <moon.2060@gmail.com>
2024-10-27 09:19:28 +0300

    'adding relations'

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'due_date',
+        'status',
+        'user_id',
+    ];
+
+    /**
+     * Get the user that owns the task.
+     */
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
